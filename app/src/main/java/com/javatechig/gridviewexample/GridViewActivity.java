1020aa5de0d90970fca61845b85a76d2ff3ea2ff
Joonhee <pakjoonhee@gmail.com>
2016-05-26 22:03:44 -0700

    Add 2 buttons

--- app/src/main/java/com/javatechig/gridviewexample/GridViewActivity.java
+++ app/src/main/java/com/javatechig/gridviewexample/GridViewActivity.java
@@ -12,6 +12,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -27,7 +29,6 @@ public class GridViewActivity extends ActionBarActivity {
 
     private GridViewAdapter mGridAdapter;
     private ArrayList<GridItem> mGridData;
-    private String FEED_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a247f9509512beb8588090c3d377d6c9";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +58,6 @@ public class GridViewActivity extends ActionBarActivity {
             }
         });
 
-        new AsyncHttpTask().execute(FEED_URL);
     }
 
     public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
@@ -135,4 +135,35 @@ public class GridViewActivity extends ActionBarActivity {
             e.printStackTrace();
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.popular_movies) {
+            new AsyncHttpTask().execute("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a247f9509512beb8588090c3d377d6c9");
+            return true;
+        }
+
+        if (id == R.id.highest_rated) {
+            new AsyncHttpTask().execute("http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&api_key=a247f9509512beb8588090c3d377d6c9");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
